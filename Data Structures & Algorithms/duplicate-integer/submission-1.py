e72090c9ea5c5512f592bcd432e0eaf5e4994882
Bantamlak Tilahun <106753253+Bantamlak12@users.noreply.github.com>
2026-09-07 16:36:25 +0300

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        dup = []
+        for num in nums:
+            if num in dup:
+                return True
+            dup.append(num)
+        return False
