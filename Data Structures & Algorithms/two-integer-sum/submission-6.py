f6dfaf5f94c82c13fc4e07a04139e250f750d6b6
Bantamlak Tilahun <106753253+Bantamlak12@users.noreply.github.com>
2026-09-08 09:07:04 +0300

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for idx, value in enumerate(nums):
+            complement = target - value
+            if complement in hashmap:
+                return [hashmap[complement], idx]
+            hashmap[value] = idx
